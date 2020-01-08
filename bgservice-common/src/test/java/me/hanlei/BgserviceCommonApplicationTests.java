62e505600bd3ca3ba35087e0cc223f76a0843553
hanlei_csd <hanlei_csd@si-tech.com.cn>
2020-01-08 17:32:33 +0800

    Initial commit

---
+++ bgservice-common/src/test/java/me/hanlei/BgserviceCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hanlei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgserviceCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
